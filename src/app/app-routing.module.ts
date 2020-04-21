37073cfed259734a76a5d478886cee40070567db
SajanKashi <sajan.kashi@nineleaps.com>
2020-04-21 12:26:34 +0530

    Sajan - Adding routing logic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HeroesComponent }      from './heroes/heroes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes),
+    CommonModule  
+  ],
+  exports: [RouterModule],
+  declarations : []
 })
 export class AppRoutingModule { }
